ef48161465f91f09a77cee965c761b8be029ddf4
DanielChekal <Danielchekal@Gmail.com>
2018-06-04 09:56:35 -0700

    MyMapsApp

--- app/src/main/java/com/example/chekald3384/mymapsapps/MapsActivity.java
+++ app/src/main/java/com/example/chekald3384/mymapsapps/MapsActivity.java
@@ -63,7 +63,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION != PackageManager.PERMISSION_GRANTED)){
                 (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION == PackageManager.permission.ACCESS_COARSE_LOCATION);
-            mMap.setMyLocationEnabled(true));
+            mMap.setMyLocationEnabled(true);
         }
 
     }
